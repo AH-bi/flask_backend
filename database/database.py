fee913bce487399a0cc7dd9d5f24159224597762
AH-bi <ahmedbinous404@gmail.com>
2024-01-22 22:34:51 -0600

    updata database configuration

--- database/database.py
+++ database/database.py
@@ -6,9 +6,8 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3309/tp_cicd'
-    # The database configuration to use Heroku's DATABASE_URL
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql://root:@127.0.0.1:3309/tp_cicd')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3309/tp_cicd'
+
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
