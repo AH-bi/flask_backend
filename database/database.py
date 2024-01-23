52bd3f6461f83ab88655550fad4dbdb81b3dc6e6
AH-bi <ahmedbinous404@gmail.com>
2024-01-22 22:23:39 -0600

    Fix Procfile syntax

--- database/database.py
+++ database/database.py
@@ -1,11 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask 
-
+import os 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3309/tp_cicd'
+    
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3309/tp_cicd'
+    # The database configuration to use Heroku's DATABASE_URL
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql://root:@127.0.0.1:3309/tp_cicd')
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
